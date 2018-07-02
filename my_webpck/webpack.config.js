5b09bf2b96554abc9e5d37cab09a81956b152bdf
zuobaiquan <zuobaiquan@qq.com>
2018-07-02 15:10:05 +0800

    ddd

--- my_webpck/webpack.config.js
+++ my_webpck/webpack.config.js
@@ -62,10 +62,5 @@ module.exports = {
       new HtmlWebpackPlugin({
         template: 'index.html'
       })
-    ],
-    devServer: {
-      contentBase: "./",//本地服务器所加载的页面所在的目录
-      historyApiFallback: true,//不跳转
-      inline: true//实时刷新
-    }
+    ]
 }
