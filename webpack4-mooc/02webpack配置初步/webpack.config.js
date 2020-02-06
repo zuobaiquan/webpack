c67ec059bbbf5956366606780e1204b780e19332
zuobaiquan <zuobaiquan01@gmail.com>
2020-02-06 21:49:04 +0800

    update

--- webpack4-mooc/02webpack配置初步/webpack.config.js
+++ webpack4-mooc/02webpack配置初步/webpack.config.js
@@ -2,11 +2,12 @@
 const path=require('path')
 
 module.exports={
+  mode:'development', // mode是 development时，webpack不会压缩的；是production时会压缩
   entry:'./src/index.js',
   output:{
     filename:'bundle.js',
     // path.resolve 绝对路径
     // __dirname 绝对路径
-    path:path.resolve(__dirname,'bundle')
+    path:path.resolve(__dirname,'dist')
   }
 }
--- webpack4-mooc/README.md
+++ webpack4-mooc/README.md
@@ -2,7 +2,10 @@ webpack 中文文档 https://www.webpackjs.com/concepts/
 
 webpack 英文文档 https://webpack.js.org/concepts/
 
-
 npm info webpack 查看webpack历史版本
 
 webpack 默认配置文件 webpack.config.js
+如果想修改默认配置文件名称为 webpackconfig.js ，只需要执行命令： npx webpack --config webpackconfig.js
+
+
+file-loader 配置 https://webpack.js.org/loaders/file-loader/#root
